1220801280a3eb63dbbf558207ef8dd8c91a31e7
mingoo <country1207@naver.com>
2024-07-25 16:20:05 +0900

    민근 25 16:20

--- H_Project/project1/routes/userRouter.js
+++ H_Project/project1/routes/userRouter.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const conn = require("../config/db")
 
-// 1. 로그인로직 
-router.post("/join",(req,res)=>{
+// 1. 회원가입 로직 
+router.post("/signup",(req,res)=>{
     let {id,pw,nick} = req.body;
     let sql = "insert into member values (?,?,?)"
     conn.query(spl,[id,pw,nick],(err,rows)=>{
