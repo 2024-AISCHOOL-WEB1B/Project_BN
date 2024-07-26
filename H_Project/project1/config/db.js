bb6dde5d76b46e52375600f46da5ddc8756ad7c8
mingoo <country1207@naver.com>
2024-07-26 11:20:02 +0900

    민근 26 11:19

--- H_Project/project1/config/db.js
+++ H_Project/project1/config/db.js
@@ -1,1 +1,15 @@
 // DB 연동 파일
+const mysql = require("mysql")
+
+const conn = mysql.createConnection({
+    host : ,
+    port : ,
+    user : ,
+    password : ,
+    database : ""
+})
+
+//연결 진행
+conn.connect();
+
+module.exports = conn;
