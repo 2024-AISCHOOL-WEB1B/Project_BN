3900a4deceaa501c010b6b2575cad5b583153903
mingoo <country1207@naver.com>
2024-07-26 14:37:05 +0900

    민근 26 14:37

--- H_Project/project1/app.js
+++ H_Project/project1/app.js
@@ -31,30 +31,45 @@ nunjucks.configure("views", {
 })
 
 
-// 샘플 음식점 데이터
+// 일식 음식점 데이터
 const restaurants = [
-    { id: 1, name: '이탈리안 레스토랑', cuisine: '이탈리안', rating: 4.5 },
-    { id: 2, name: '중국 음식점', cuisine: '중국', rating: 4.0 },
-    { id: 3, name: '일본 스시', cuisine: '일본', rating: 5.0 },
-    { id: 4, name: '한국 BBQ', cuisine: '한국', rating: 4.8 },
-    { id: 5, name: '멕시코 타코', cuisine: '멕시코', rating: 4.2 }
+    { id: 1, name: '규슈', cuisine: '일식', review: 3528 },
+    { id: 2, name: '목하식당', cuisine: '일식', review: 2688 },
+    { id: 3, name: '캬베츠', cuisine: '일식', review: 1971 },
+    { id: 4, name: '동백카츠', cuisine: '일식', review: 1051 },
+    { id: 5, name: '구미구미', cuisine: '일식', review: 721 },
+    { id: 6, name: '돈부리바쇼 유메노덴', cuisine: '일식', review: 811 },
+    { id: 7, name: '연어쁘다', cuisine: '일식', review: 3749 },
+    { id: 8, name: '포카포카', cuisine: '일식', review: 556 },
+    { id: 9, name: '달곰식당', cuisine: '일식', review: 608 },
+    { id: 10, name: '스시야스라기', cuisine: '일식', review: 1024 },
+    { id: 11, name: '이츠모', cuisine: '일식', review: 1335 },
+    { id: 12, name: '지은초밥', cuisine: '일식', review: 1175 },
+    { id: 13, name: '윤끼', cuisine: '일식', review:475 },
+    { id: 14, name: '소보쿠', cuisine: '일식', review: 419 },
+    { id: 15, name: '천지라멘', cuisine: '일식', review: 566 },
+    { id: 16, name: '소바 쿄다이', cuisine: '일식', review: 796 },
+    { id: 17, name: '미노라멘', cuisine: '일식', review: 571 },
+    { id: 18, name: '멘지', cuisine: '일식', review: 405 },
+    { id: 19, name: '송경솥밥', cuisine: '일식', review: 101 },
+    { id: 20, name: '아라타', cuisine: '일식', review: 288 }
 ];
 
 // 사용자 선호도 예시 (cuisine: 음식 종류)
 const userPreferences = {
-    cuisine: '일본', // 사용자가 선호하는 음식 종류
+    cuisine: '일식', // 사용자가 선호하는 음식 종류
 };
 
 // 추천 알고리즘
 function recommendRestaurants(preferences) {
-    return restaurants.filter(restaurant => restaurant.cuisine === preferences.cuisine);
+    return restaurants.filter(restaurant => restaurant.cuisine < preferences.review);
 }
 
 // API 엔드포인트
-app.get('/recommend', (req, res) => {
-    const recommendations = recommendRestaurants(userPreferences);
-    res.json(recommendations);
-});
+//app.get('/recommend', (req, res) => {
+   // const recommendations = recommendRestaurants(userPreferences);
+   // res.json(recommendations);
+//});
 
 
 app.listen(3000);
