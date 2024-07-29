3841d3869b415fce143363ac9b60d7068f3713cb
mingoo <country1207@naver.com>
2024-07-29 14:55:52 +0900

    민근 29 14:55

--- H_Project/project1/config/db.js
+++ H_Project/project1/config/db.js
@@ -1,16 +1,15 @@
 // DB 연동 파일
-// const mysql = require("mysql")
+const mysql = require("mysql")
 
-// const conn = mysql.createConnection({
-//     host : ,
-//     port : ,
-//     user : ,
-//     password : ,
-//     database : ""
-// })
+const conn = mysql.createConnection({
+     host : project-db-stu3.smhrd.com,
+     port : 3307,
+     user : Insa5_JSB_hacksim_3,
+     password : aischool3,
+     database : "node.js"
+ })
 
 // //연결 진행
-// conn.connect();
-// console.log("DB 연결 완료")
-
-// module.exports = conn;
+ conn.connect();
+ console.log("DB 연결 완료")
+ module.exports = conn;
