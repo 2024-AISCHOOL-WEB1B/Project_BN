c2c137e3c6ed9b7737f6521c27561284a064f650
mingoo <country1207@naver.com>
2024-07-30 15:37:10 +0900

    민근 30 15:37

--- H_Project/project1/routes/ownerRouter.js
+++ H_Project/project1/routes/ownerRouter.js
@@ -2,5 +2,21 @@
 // const router = express.Router();
 // const conn = require("../config/db")
 
+// 사용자가 회원가입을 요청했을 때
+// router.get("/register", (req,res)=>{
+//     res.render("register")
+// })
+
+// // 사용자가 로그인을 요청했을 때
+// router.get("/login", (req,res)=>{
+//     res.render("login")
+// })
+
+// // 사용자가 로그아웃을 요청했을 때
+// router.get("/logout",(req,res)=>{
+//     res.render("logout")
+// })
+
+
 
 // module.exports = router;
