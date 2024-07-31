cc247710bb7ccbf3a9c6462542b6ebea15ca11e5
henwooman <gcoin09@gmail.com>
2024-07-31 10:09:56 +0900

    07/31  10:09

--- H_Project/project1/routes/ownerRouter.js
+++ H_Project/project1/routes/ownerRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const conn = require("../config/db")
 
 // 오너가 회원가입을 요청했을 때
-router.post("/ownerRegister", (req,res)=>{
+router.post("/register", (req,res)=>{
     let { owner_id, owner_pw, store_name, business_registration_num, email } = req.body;
     let sql = "insert into owners values (?,?,?,?,?)"
     let = [
@@ -16,61 +16,39 @@ router.post("/ownerRegister", (req,res)=>{
     conn.query(sql, [owner_id, owner_pw, store_name, business_registration_num, email],(err, rows)=>{
         console.log("DB insert:", rows);
 
-        if(rows){
+        if (rows) {
             console.log("점주 회원가입 성공");
-            res.redirect("/ownerpage")
-        }else{
+            res.redirect("/")
+        } else {
             res.send("<script>alert('회원가입 실패')</script>")
         }
     })
 })
 
-
-
-
-
 // 오너가 로그인을 요청했을 때
-router.get("/login", (req,res)=>{
-    res.render("login")
+router.post("/login", (req,res)=>{
+    let {id, pw} = req.body;
+    let sql = "select * from owners where owner_id = ? and owner_pw =?"
+    let = [id,pw]
+    conn.query(sql, [id,pw], (err, rows)=>{
+        if(rows.length > 0) {
+            console.log("점주 로그인 성공");
+            req.session.store_name = rows[0].store_name;
+            req.session.email = rows[0].email;
+            res.render("ownerpage")
+        } else {
+            console.log("점주 로그인 실패")
+            res.send("<script>alert('로그인 실패')</script>")
+        }
+    })
+    
 })
 
 // 오너가 로그아웃을 요청했을 때
 router.get("/logout",(req,res)=>{
-    res.render("logout")
+    req.session.destroy();
+    res.redirect("/")
 })
-
-// 마이페이지 이동
-router.get("/mypage", (req,res)=>{
-    if(req.session.nick){
-        res.render("mypage", { 
-            nick: req.session.nick,
-            email: req.session.email
-         });
-    } else {
-        res.render("mypage")
-    } 
-    console.log("마이페이지 이동")
-})
-
-
-// 사용자가 정보수정을 요청했을 때
-router.get("/updateRegister", (req,res)=>{
-    if(req.session.nick){
-        res.render("updateRegister", { 
-            nick: req.session.nick,
-            email: req.session.email
-         });
-    } else {
-        res.render("updateRegister")
-    } 
-    console.log("정보수정페이지 이동")
-})
-
-// 사용자가 회원탈퇴를 요청했을 때
-router.get("/deleteAccount", (req,res)=>{
-    res.render("deleteAccount")
-})
-
 
 
 module.exports = router;
